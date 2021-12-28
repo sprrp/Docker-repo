2e7888015fd97b6f45969d3fd566bcd6a2c2ae83
intelycorellc <intelycorellc@intelycellcsMBP.fios-router.home>
2021-12-28 15:36:54 -0500

    added files

---
+++ java-api-docker/src/test/java/com/sswork/echo/EchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sswork.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
